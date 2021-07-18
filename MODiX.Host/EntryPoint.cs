b045dbb24c8061ac774e4376ef4c2470363ff7d4
JakenVeina <jakenveina@yahoo.com>
2021-07-17 23:14:17 -0500

    Added support for docker secrets

--- MODiX.Host/EntryPoint.cs
+++ MODiX.Host/EntryPoint.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System.IO;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 using Modix.Host.Logging;
@@ -14,6 +17,12 @@ namespace Modix.Host
                 {
                     options.ValidateOnBuild = context.HostingEnvironment.IsDevelopment();
                     options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                })
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    var keyPerFilePath = context.Configuration.GetSection("MODiX:Host:SecretsFilesPath").Value;
+                    if (!string.IsNullOrWhiteSpace(keyPerFilePath))
+                        builder.AddKeyPerFile(keyPerFilePath);
                 })
                 .ConfigureWebHostDefaults(webHost => webHost
                     .UseStartup<Startup>())
