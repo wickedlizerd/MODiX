7afe4192c63ac89dca2d6cce2085c28c0257e6de
JakenVeina <jakenveina@yahoo.com>
2021-10-21 03:33:34 -0500

    Consolidated HostBuilder and Startup logic

--- MODiX.Host/EntryPoint.cs
+++ MODiX.Host/EntryPoint.cs
@@ -1,34 +1,53 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Reactive.PlatformServices;
 using System.Reflection;
 using System.Text.Json.Serialization;
 
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+using Modix.Bot;
+using Modix.Business;
+using Modix.Data;
+using Modix.Data.Migrations;
+using Modix.Web.Server;
+
 namespace Modix.Host
 {
-    public static class EntryPoint
+    public class EntryPoint
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            using var host = Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+            using var host = new HostBuilder()
                 .UseDefaultServiceProvider((context, options) =>
                 {
                     options.ValidateOnBuild = context.HostingEnvironment.IsDevelopment();
-                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                    options.ValidateScopes  = context.HostingEnvironment.IsDevelopment();
                 })
+                .ConfigureHostConfiguration(builder => builder
+                    .AddEnvironmentVariables(prefix: "DOTNET_"))
                 .ConfigureAppConfiguration((context, builder) =>
                 {
+                    builder
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+
+                    if (context.HostingEnvironment.IsDevelopment())
+                        builder.AddUserSecrets<EntryPoint>(optional: true);
+
                     var secretsFilesPath = context.Configuration["SECRETS_FILES_PATH"];
                     if (!string.IsNullOrWhiteSpace(secretsFilesPath))
                         builder.AddKeyPerFile(secretsFilesPath);
                 })
-                .ConfigureWebHostDefaults(webHost => webHost
-                    .UseStartup<Startup>())
                 .ConfigureLogging((context, builder) => builder
                     .AddConfiguration(context.Configuration.GetSection("MODiX:Host:Logging"))
                     .AddConsole()
@@ -61,6 +80,40 @@ namespace Modix.Host
                                 options.ReferenceHandler = ReferenceHandler.Preserve;
                                 options.Converters.Add(new NullableContextAttributeWriteOnlyJsonConverter());
                             })))
+                .ConfigureServices((context, services) => services
+                    .AddResponseCompression(options => options
+                        .MimeTypes = ResponseCompressionDefaults.MimeTypes
+                            .Append("application/octet-stream"))
+                    .AddSingleton<ISystemClock, DefaultSystemClock>()
+                    .AddModixBot()
+                    .AddModixBusiness(context.Configuration)
+                    .AddModixData(context.Configuration)
+                    .AddModixDataMigrations()
+                    .AddModixWebServer(context.Configuration))
+                .ConfigureWebHost(builder => builder
+                    .UseKestrel((context, options) => options
+                        .Configure(context.Configuration.GetSection("Kestrel")))
+                    .ConfigureAppConfiguration((context, _) =>
+                    {
+                        if (context.HostingEnvironment.IsDevelopment())
+                            StaticWebAssetsLoader.UseStaticWebAssets(context.HostingEnvironment, context.Configuration);
+                    })
+                    .Configure((context, builder) =>
+                    {
+                        builder
+                            .UseResponseCompression();
+
+                        if (context.HostingEnvironment.IsDevelopment())
+                            builder
+                                .UseDeveloperExceptionPage()
+                                .UseWebAssemblyDebugging();
+
+                        builder
+                            .UseBlazorFrameworkFiles()
+                            .UseStaticFiles()
+                            .UseModixWebServer()
+                            .UseFallbackFile("index.html");
+                    }))
                 .Build();
 
             host.Run();
